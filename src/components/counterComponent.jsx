19086f6816f7a6fd4b76a17cff629cac823914b0
Krishna <krishna.t@gmail.com>
2022-03-05 20:10:30 -0500

    Adding Dynamic Classes

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0
+        count: 1
     }
 
     formatCount() {
@@ -10,13 +10,21 @@ class Counter extends Component {
         return count == 0 ? 'Zero' : count;
     }
 
+    
+
     render() {
         return (
             <div>
-                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count > 0 ? 'primary' : 'info';
+        return classes;
     }
 }
 
