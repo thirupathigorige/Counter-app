8bb0df55aa3b3229497889ecd8bef0fe4d873418
Krishna <krishna.t@gmail.com>
2022-03-05 20:02:19 -0500

    Creating function and calling function

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    render() { 
-        return <h1>Hello Word!</h1>;
+    state = {
+        count: 0
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count == 0 ? 'Zero' : count;
+    }
+
+    render() {
+        return (
+            <div>
+                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
+            </div>
+        );
     }
 }
- 
+
 export default Counter;
